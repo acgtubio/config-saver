2486a675e578a83f5d0ff7d249d85e21d5112c92
Adrian <adrian.tubio@outlook.com>
2024-07-04 00:21:02 +0800

    Add basic structure for commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Init,
+    TrackFile {
+        #[arg(short, long)]
+        filename: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Init => {
+            println!("Initializing directory...");
+        }
+        Command::TrackFile { filename } => {
+            println!("Tracking: {}", filename);
+        }
+    }
 }
