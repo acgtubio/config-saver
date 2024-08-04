3acd18a05b78a33fe5d063afbcd256b01b16d02b
Adrian <adrian.tubio@outlook.com>
2024-08-04 22:40:54 +0800

    add reset functionality

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::{fs, io::Error, path::Path};
 struct Args {
     #[command(subcommand)]
     command: Command,
+    #[arg(short, long, default_value = "")]
+    mode: String,
 }
 
 #[derive(Debug, Subcommand)]
@@ -27,7 +29,7 @@ fn main() {
             print!("showing config...\n");
         }
         Command::Init => {
-            init_handler();
+            init_handler(args.mode);
         }
         Command::TrackFile { filename } => {
             println!("Tracking: {}", filename);
@@ -58,14 +60,26 @@ fn create_proj_config_file() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn init_handler() {
+fn init_handler(mode: String) {
     if !is_config_initialized() {
         create_config_location();
     }
 
     if proj_config_file_exists() {
         println!("Config already exists...");
-        return;
+
+        // Temporarily set as comment due to testing code below.
+        // return;
+
+        // We remove config files when using it as testing.
+        if mode == "debug" {
+            reset_handler();
+        } else if mode != "debug" && mode != "" {
+            println!("unknown mode...");
+            return;
+        } else {
+            return;
+        }
     }
 
     if let Err(..) = create_proj_config_file() {
@@ -74,6 +88,7 @@ fn init_handler() {
 }
 
 fn reset_handler() {
+    println!("Removing config file...");
     if !proj_config_file_exists() {
         return;
     }
